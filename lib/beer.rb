f09184e0fb82cd226aa20fe8d8ae762c91ee8d04
jstricklin89 <jstricklin89@gmail.com>
2018-09-11 16:17:19 -0500

    fixed index in beers and breweries csv files

--- lib/beer.rb
+++ lib/beer.rb
@@ -4,4 +4,12 @@ class Beer < ActiveRecord::Base
   belongs_to :category
   has_many :favorites
   has_many :users, through: :favorites
+
+  def self.most_popular_beer
+    # use id, use max_by on array, use count(id)
+  end
+
+  def self.average_abv
+
+  end
 end
