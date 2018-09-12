2f0c6de70af07c3b923a32f7e59aef8087446fdc
jstricklin89 <jstricklin89@gmail.com>
2018-09-12 15:14:55 -0500

    added most popular methods to brewery, style, and category classes

--- lib/beer.rb
+++ lib/beer.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class Beer < ActiveRecord::Base
   belongs_to :style
   belongs_to :brewery
@@ -6,6 +7,27 @@ class Beer < ActiveRecord::Base
   has_many :users, through: :favorites
 
   def self.most_popular(num)
-    Favorite.all.map { |favorite| favorite.beer_id }
+    id_count = get_id_count(Favorite.all)
+
+    id_count.keys.first(num).map do |id|
+      Beer.find(id)
+    end
   end
+  
+  def self.get_id_count(collection)
+    id_array = collection.map do |item|
+      item.beer_id
+    end
+    
+    id_count = id_array.each_with_object(Hash.new(0)) do |id, count|
+      count[id] += 1
+    end
+    
+    id_count = id_count.sort_by do |k, v|
+      v
+    end.reverse.to_h
+  end
+  private_class_method :get_id_count
 end
+
+
--- lib/category.rb
+++ lib/category.rb
@@ -4,4 +4,31 @@ class Category < ActiveRecord::Base
   has_many :breweries, through: :beers
   has_many :users, through: :beers
   has_many :favorites, through: :beers
+
+  def self.most_popular(num)
+    id_count = get_id_count(Favorite.all)
+
+    id_count.keys.first(num).map do |id|
+      Category.find(id)
+    end
+  end
+  
+  def self.get_id_count(collection)
+    id_array = collection.map do |fav|
+      fav.beer_id
+    end
+    
+    cat_array = id_array.map do |id|
+      Beer.find(id).category_id
+    end
+
+    id_count = cat_array.each_with_object(Hash.new(0)) do |id, count|
+      count[id] += 1
+    end
+    
+    id_count = id_count.sort_by do |k, v|
+      v
+    end.reverse.to_h
+  end
+  private_class_method :get_id_count
 end
